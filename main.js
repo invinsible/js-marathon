c76e9a6ed64e9c53bf4ccf1438a62d9f4e5a2c50
Sergey Butyanov <igor@tomatstudio.ru>
2020-09-24 11:01:43 +0300

    HomeWork 2

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('r2d2');
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+function getRow(firstRow, secondRow) {    
+    let numsFirst = 0;
+    let numsSecond = 0;
+
+    for(let i = 0; i < firstRow.length; i++) {
+        if (firstRow.charAt(i) == 'а') {
+            numsFirst += 1;
+        }
+    }
+
+    for(let j = 0; j < secondRow.length; j++) {
+        if (secondRow.charAt(j) == 'а') {
+            numsSecond += 1;
+        }
+    }
+
+    if (numsFirst > numsSecond) {
+        return firstRow
+    } else if (numsFirst === numsSecond) {
+        return 'Строки равны'
+    } else {
+        return secondRow
+    }
+
+    
+}
+
+console.log(getRow(firstRow, secondRow));
