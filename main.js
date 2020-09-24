8c3082c6172283a9be28afee17c7a440a00a87ef
Sergey Butyanov <igor@tomatstudio.ru>
2020-09-24 11:18:14 +0300

    Bonus for task 1

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+const firstRow = prompt('Введите первую строку');
+const secondRow =  prompt('Введите вторую строку');
+const marker = prompt('Какую букву будем считать?');
 
 function getRow(firstRow, secondRow) {    
     let numsFirst = 0;
     let numsSecond = 0;
 
     for(let i = 0; i < firstRow.length; i++) {
-        if (firstRow.charAt(i) == 'а') {
+        if (firstRow.charAt(i) == marker) {
             numsFirst += 1;
         }
     }
 
     for(let j = 0; j < secondRow.length; j++) {
-        if (secondRow.charAt(j) == 'а') {
+        if (secondRow.charAt(j) == marker) {
             numsSecond += 1;
         }
     }
 
     if (numsFirst > numsSecond) {
-        return firstRow
+        return 'В слове "' + firstRow + '" больше букв ' + marker;
     } else if (numsFirst === numsSecond) {
-        return 'Строки равны'
+        return 'Строки равны по букве ' + marker;
     } else {
-        return secondRow
-    }
-
-    
+        return 'В слове "' + secondRow + '" больше букв ' + marker;
+    }    
 }
 
 console.log(getRow(firstRow, secondRow));
